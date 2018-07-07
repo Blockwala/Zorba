e3b9d2d707f567ecbff080d99d09f3d5f7f21ff3
karan <karan@blockwala.io>
2018-07-07 20:41:31 +0530

    ERC20 APIS ready

--- api/ERC20contracts/index.js
+++ api/ERC20contracts/index.js
@@ -6,5 +6,9 @@ contracts = require('./controller.js');
 
 // GET
 router.get('/getContractBalance/:contract_address/:user_address', contracts.getContractBalance);
+router.get('/getTicker/:contract_address', contracts.getTickerOfCoin);
+router.get('/getName/:contract_address', contracts.getNameOfCoin);
+router.get('/getDecimals/:contract_address', contracts.getDecimalOfCoin);
+
 
 module.exports = router;
