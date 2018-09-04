c60c7fef737da03fe539ce10a9df7d5aee8122d0
karan <karan@blockwala.io>
2018-09-04 20:13:40 +0530

    smarter way

--- scripts/timestamp_fix_new.js
+++ scripts/timestamp_fix_new.js
@@ -21,11 +21,11 @@ start = function() {
 	var collection = dbo.collection("transfers");
 	var blockCollection = dbo.collection("blocks");
 
-	collection //change collection here
+	blockCollection //change collection here
 	.count()
 	.then(function(response) {
 		console.log(response)
-		var bucket_size = 10000
+		var bucket_size = 1000
 		var buckets = Number(response)/bucket_size
 		var index = []
 		console.log(buckets)
@@ -43,30 +43,19 @@ start = function() {
 			console.log(skip)
 
 			var cursor = 
-			collection
+			blockCollection
 			.find({})
 			.limit(limit)
 			.skip(skip)
-			.sort({"blockNumber": 1}); //make sure this is indexed
+			.sort({"timestamp": 1}); //make sure this is indexed
 
 			cursor.forEach(
 				function(doc) {
-
-					blockCollection
-					.find({"number": doc.blockNumber}, {"timestamp":1})
-					.toArray(function(err, res) {
-						block = res[0]
-						if(block != undefined && block.timestamp != undefined) {
-							doc.timestamp = block.timestamp
-
-							console.log(doc.timestamp) //change var if collection change
-
-							collection
-							.update({'hash': doc.hash}, doc, {upsert: true}) //change var if collection change
-						}
-						
-					});
-
+					console.log(doc.timestamp);
+					console.log(doc.number)
+					collection.updateMany({"blockNumber": doc.number}, {"$set":{timestamp: doc.timestamp}},  { upsert: true })
+					var waitTill = new Date(new Date().getTime() + .05 * 1000);
+					while(waitTill > new Date()){}
 				},
 				function(err) { 
 
